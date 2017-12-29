6dba0b4f5ebc6731b7b4eec484ab0c985e0c9aa8
ZJNU-Hmz <1134598796@qq.com>
2017-12-29 18:25:42 +0800

    Initial commit

---
+++ src/main/java/com/controller/PageController.java
@@ -0,0 +1,23 @@
+package com.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class PageController {
+
+
+    @RequestMapping("/")
+    public String orign() {
+        return "index";
+    }
+
+
+
+}
