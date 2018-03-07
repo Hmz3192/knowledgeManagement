602bb4d4468e7b19289eca5c3f4949a6e5195ea2
Charles <smallpj73@gmail.com>
2018-03-07 21:06:10 +0800

    3.7

--- src/main/java/com/dao/KlUserManagerMapper.java
+++ src/main/java/com/dao/KlUserManagerMapper.java
@@ -1,13 +1,25 @@
 package com.dao;
 
+import com.model.KlManager;
 import com.model.KlUser;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * Created by dellpc on 2018/3/5.
  */
 public interface KlUserManagerMapper {
 
-    public KlUser findUserByNameAndPwd(@Param("u_name") String u_name, @Param("password") String password);
+    public KlManager findUserByNameAndPwd(@Param("m_name") String m_name, @Param("password") String password);
 
+    public KlUser selectUser(@Param("u_name") String u_name);
+
+    public List<KlUser> selectAll();
+
+    public int addUser(KlUser klUser);
+
+    public int delUser(int userId);
+
+    public int updateUser(KlUser klUser);
 }
