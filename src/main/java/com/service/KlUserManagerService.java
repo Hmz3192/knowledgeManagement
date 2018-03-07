726c6b212e5c427b0ec82fb2e7a157de3d2ee349
Charles <smallpj73@gmail.com>
2018-03-07 21:06:58 +0800

    3.7

--- src/main/java/com/service/KlUserManagerService.java
+++ src/main/java/com/service/KlUserManagerService.java
@@ -1,11 +1,19 @@
 package com.service;
 
+import com.model.KlManager;
 import com.model.KlUser;
+
+import java.util.List;
 
 /**
  * Created by dellpc on 2018/3/5.
  */
 public interface KlUserManagerService {
 
-    KlUser managerLogin(String u_name, String password);
+    public KlManager managerLogin(String m_name, String password);
+    public KlUser findUserByName(String userName);
+    public List<KlUser> findAll();
+    public int addUser(KlUser klUser);
+    public int delUser(int userId);
+    public int updateUser(KlUser klUser);
 }
