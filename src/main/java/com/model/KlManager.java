fe816298f1095ec59d7d8ee8978fbab7a31ebcff
Charles <smallpj73@gmail.com>
2018-03-07 21:06:26 +0800

    3.7

---
+++ src/main/java/com/model/KlManager.java
@@ -0,0 +1,35 @@
+package com.model;
+
+/**
+ * Created by dellpc on 2018/3/5.
+ */
+public class KlManager {
+
+    private int id;
+    private String name;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
