46a73ceb53de849617004e09e4702c765060549e
Charles <smallpj73@gmail.com>
2018-03-08 20:40:37 +0800

    管理员对用户进行管理

--- src/main/java/com/service/impl/KlUserManagerServiceImpl.java
+++ src/main/java/com/service/impl/KlUserManagerServiceImpl.java
@@ -1,8 +1,11 @@
 package com.service.impl;
 
-import com.dao.KlUserManagerMapper;
-import com.model.KlManager;
+import com.dao.KlUserMapper;
+import com.dao.ManagerMapper;
+import com.model.KlUserExample;
+import com.model.Manager;
 import com.model.KlUser;
+import com.model.ManagerExample;
 import com.service.KlUserManagerService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,49 +24,49 @@ public class KlUserManagerServiceImpl implements KlUserManagerService{
     private static Logger log = Logger.getLogger(KlUserManagerServiceImpl.class);
 
     @Autowired
-    private KlUserManagerMapper klUserManagerMapper;
+    private ManagerMapper klUserManagerMapper;
+    @Autowired
+    private KlUserMapper klUserMapper;
+
+//    @Override
+//    public Manager managerLogin(String m_name, String password) {
+//        Manager klManager = klUserManagerMapper.findUserByNameAndPwd(m_name,password);
+//        if (klManager != null) {
+//            return klManager;
+//        }
+//        return null;
+//    }
 
     @Override
-    public KlManager managerLogin(String m_name, String password) {
-        KlManager klManager = klUserManagerMapper.findUserByNameAndPwd(m_name,password);
-        if (klManager != null) {
-            return klManager;
-        }
-        return null;
-    }
-
-    @Override
-    public KlUser findUserByName(String u_name) {
-        KlUser klUser = klUserManagerMapper.selectUser(u_name);
-
-        log.info("userName:"+klUser.getUserName());
-        if (klUser != null) {
-            return klUser;
-        }
-        return null;
+    public KlUser selectByPrimaryKey(Integer userId) {
+        return klUserMapper.selectByPrimaryKey(userId);
     }
 
     @Override
     public List<KlUser> findAll() {
-        List<KlUser> klUserList = klUserManagerMapper.selectAll();
-        return klUserList;
+        KlUserExample example = new KlUserExample();
+        KlUserExample.Criteria criteria = example.createCriteria();
+        return klUserMapper.selectByExample(example);
     }
 
     @Override
     public int addUser(KlUser klUser) {
-        int newUserId = klUserManagerMapper.addUser(klUser);
+        int newUserId = klUserMapper.insert(klUser);
         return newUserId;
     }
 
     @Override
     public int delUser(int userId) {
-        int result = klUserManagerMapper.delUser(userId);
+        int result = klUserMapper.deleteByPrimaryKey(userId);
         return result;
     }
 
     @Override
     public int updateUser(KlUser klUser) {
-        int result = klUserManagerMapper.updateUser(klUser);
+        KlUserExample example = new KlUserExample();
+        KlUserExample.Criteria criteria = example.createCriteria();
+        criteria.andUserIdEqualTo(klUser.getUserId());
+        int result = klUserMapper.updateByExampleSelective(klUser,example);
         return result;
     }
 }
